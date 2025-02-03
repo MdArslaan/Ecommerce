8688902231881b3811de5eab89e5645fce106e2a
MdArslaan <142446633+MdArslaan@users.noreply.github.com>
2025-02-03 15:28:59 +0530

    Backend Done

--- server/server.js
+++ server/server.js
@@ -37,8 +37,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    // origin: "*", // Allow all origins
+    // origin: "http://localhost:5173",
+    origin: "https://ecommerce-two-psi-33.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
